ea53cc1316a78e3812cd7324fe80f7c028126318
Molochko1990 <Es01@bk.ru>
2023-01-14 15:03:54 +0500

    Сделал программу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input = InputStr("Введите данные: ");
+
+string[] array = input.Split(new char[]{' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+string[] arrayNew = new string[array.Count()];
+int ind = 0;
+
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i].Length < 4) 
+    {
+        arrayNew[ind] = array[i];
+        ind++;
+    } 
+}
+ Console.WriteLine("Обновленный массив из строк: ");
+for (int i=0; i<arrayNew.Length; i++)
+{
+    Console.Write(arrayNew[i] + " ");
+}
+
+
+string InputStr(string output)
+{
+    Console.Write(output);
+    return Console.ReadLine();
+}
